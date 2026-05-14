b8bddd01e3f4caf268419bd0efe5accbb4ce754b
reafer184 <129442930+reafer184@users.noreply.github.com>
2026-05-14 14:57:58 +0300

    cleanup: remove bcrypt.ts

--- server/src/utils/bcrypt.ts
+++
@@ -1,6 +0,0 @@
-import bcrypt from 'bcrypt';
-import { env } from '../config/env.js';
-
-export const hashPassword = (password: string) => bcrypt.hash(password, env.BCRYPT_ROUNDS);
-
-export const comparePassword = (password: string, hash: string) => bcrypt.compare(password, hash);
